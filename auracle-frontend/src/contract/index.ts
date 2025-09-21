b6bec196cbef151554d895885015948430e29fdf
Shio Miharu <miharushio@gmail.com>
2025-09-21 08:55:25 +0700

    Fix API route for Vercel Serverless Function

--- auracle-frontend/src/contract/index.ts
+++ auracle-frontend/src/contract/index.ts
@@ -1,5 +1,7 @@
-import AuracleContract from '../artifacts/contracts/Auracle.sol/Auracle.json' assert { type: 'json' };
+import AuracleContract from '../artifacts/contracts/Auracle.sol/Auracle.json?url';
+
+const res = await fetch(AuracleContract);
+const abiJson = await res.json();
 
 export const AURACLE_CONTRACT_ADDRESS = import.meta.env.VITE_AURACLE_CONTRACT_ADDRESS;
-
-export const AURACLE_CONTRACT_ABI = AuracleContract.abi;
+export const AURACLE_CONTRACT_ABI = abiJson.abi;
